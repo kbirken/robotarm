ea946a57717b9a3fee844b29a7fcd82ae6aa3876
Klaus Birken <klaus.birken@gmail.com>
2015-11-28 23:59:21 +0100

    Updated library interface.

--- plugins/org.nanosite.robotarm.controller/src/org/nanosite/robotarm/controller/blackbox/IRobotArmLibrary.java
+++ plugins/org.nanosite.robotarm.controller/src/org/nanosite/robotarm/controller/blackbox/IRobotArmLibrary.java
@@ -10,11 +10,11 @@ public interface IRobotArmLibrary {
 	 * Create a SSC32 protocol command for robot arm control from various
 	 * input data (3D position, angle of attack, wrist rotation and time for the move).
 	 */
-	public abstract String getMoveCommand(double x, double y, double z, double aa, double rot, int t);
+	public String getMoveCommand(double x, double y, double z, double aa, double rot, int t);
 
 	/**
 	 * Create a SSC32 protocol command for gripper control.
 	 */
-	public abstract boolean getGrabCommand(int openMM);
+	public String getGrabCommand(int openMM);
 
 }
